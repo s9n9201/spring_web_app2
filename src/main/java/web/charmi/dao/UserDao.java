704f59f2c66e1a469f1e732fda3ff5297c763a25
kent <s9n9201@yahoo.com.tw>
2022-03-27 16:59:38 +0800

    add DAO function

--- src/main/java/web/charmi/dao/UserDao.java
+++ src/main/java/web/charmi/dao/UserDao.java
@@ -3,6 +3,9 @@ package web.charmi.dao;
 import web.charmi.entity.User;
 
 public interface UserDao {
+
+    Integer insertUser(User user);
+
     User getUser(String Value, String Column);
 
     Boolean existsUser(String Value, String Column);
--- src/main/java/web/charmi/dao/UserDaoImp.java
+++ src/main/java/web/charmi/dao/UserDaoImp.java
@@ -1,9 +1,14 @@
 package web.charmi.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import web.charmi.entity.Role;
 import web.charmi.entity.User;
 import web.charmi.rowmapper.UserRowMapper;
+import web.charmi.util.SqlMap;
 
 import java.util.HashMap;
 import java.util.List;
@@ -12,6 +17,48 @@ import java.util.Map;
 public class UserDaoImp implements UserDao {
     @Autowired
     private NamedParameterJdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private SqlMap sqlMap;
+
+    @Override
+    public Integer insertUser(User user) {
+        String SqlStr="";
+        Integer OrgId=0;
+        Map<String, Object> map=new HashMap<>();
+        Map<String, String> TableMap=new HashMap<>();
+        KeyHolder keyHolder=new GeneratedKeyHolder();
+        TableMap.put("OrgName", ":OrgName");
+        TableMap.put("Password", ":Password");
+        TableMap.put("Email", ":Email");
+        SqlStr=sqlMap.insert("Org", TableMap);
+
+        map.put("OrgName", user.getOrgName());
+        map.put("Password", user.getPassword());
+        map.put("Email", user.getEmail());
+        jdbcTemplate.update(SqlStr, new MapSqlParameterSource(map), keyHolder);
+        OrgId=keyHolder.getKey().intValue();
+
+        TableMap.clear();
+        map.clear();
+
+        List<Role> roleList=user.getRoleList();
+        MapSqlParameterSource[] parameterSource=new MapSqlParameterSource[roleList.size()];
+        TableMap.put("U_URecId", ":U_URecId");
+        TableMap.put("U_RRecId", ":u_RRecId");
+        TableMap.put("U_RoleName", ":U_RoleName");
+        SqlStr=sqlMap.insert("UserRole", TableMap);
+
+        for (int i=0; i<roleList.size(); i++) {
+            Role role=roleList.get(i);
+            parameterSource[i]=new MapSqlParameterSource();
+            parameterSource[i].addValue("U_URecId", OrgId);
+            parameterSource[i].addValue("U_RRecId", role.getRRecId());
+            parameterSource[i].addValue("U_RoleName", role.getName().name());
+        }
+        jdbcTemplate.batchUpdate(SqlStr, parameterSource);
+        return OrgId;
+    }
 
     @Override
     public User getUser(String Value, String Column) {
@@ -29,7 +76,6 @@ public class UserDaoImp implements UserDao {
 
     @Override
     public Boolean existsUser(String Value, String Column) {
-
         return getUser(Value, Column)!=null?true:false;
     }
 }
