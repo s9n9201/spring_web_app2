85e554f6072753843a958d9464e60acc5ec40c0b
kent <s9n9201@yahoo.com.tw>
2022-03-28 23:08:20 +0800

    add signIn & signup method

--- src/main/java/web/charmi/controller/UserRestController.java
+++ src/main/java/web/charmi/controller/UserRestController.java
@@ -1,4 +1,103 @@
 package web.charmi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import web.charmi.dao.RoleDao;
+import web.charmi.dao.UserDao;
+import web.charmi.entity.Role;
+import web.charmi.entity.User;
+import web.charmi.entity.enumRole;
+import web.charmi.security.jwt.JwtUtils;
+import web.charmi.security.service.UserDetailsImp;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@CrossOrigin(origins="*", maxAge=3600)
+@RestController
+@RequestMapping("/api")
 public class UserRestController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserDao userDao;
+    @Autowired
+    private RoleDao roleDao;
+    @Autowired
+    private PasswordEncoder encoder;
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> signIn(@RequestBody @Validated(User.signIn.class) User user) {
+        Map<String, Object> map=new HashMap<>();
+        Authentication authentication=authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getOrgName(),user.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt=jwtUtils.generateJwtToken(authentication);
+        UserDetailsImp userDetailsImp=(UserDetailsImp) authentication.getPrincipal();
+        List<String> roles=userDetailsImp.getAuthorities().stream()
+                            .map(item->item.getAuthority())
+                            .collect(Collectors.toList());
+        map.put("Token", jwt);
+        map.put("OrgId", userDetailsImp.getOrgId());
+        map.put("OrgName", userDetailsImp.getUsername());
+        map.put("Role", roles);
+        return ResponseEntity.ok(map);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signUp(@RequestBody @Validated(User.signUp.class) User user) {
+        String Msg="";
+        HttpStatus httpStatus=null;
+        Map<String, Object> map=new HashMap<>();
+
+        if (userDao.existsUser(user.getOrgName(), "OrgName")) {
+            httpStatus=HttpStatus.BAD_REQUEST;  //400
+            Msg="Username is already taken !";
+        } else if (userDao.existsUser(user.getEmail(), "Email")) {
+            httpStatus=HttpStatus.BAD_REQUEST;
+            Msg="Email is already in user !";
+        } else {
+            httpStatus=HttpStatus.OK;   //200
+            Msg="User registered successfully !";
+        }
+
+        if (httpStatus.equals(HttpStatus.OK)) {
+            user.setPassword(encoder.encode(user.getPassword()));
+            List<Role> strRoles=user.getRoleList();
+            List<Role> roles=null;
+            if (strRoles==null) {
+                Role userRole=roleDao.findByName(enumRole.ROLE_USER)
+                        .orElseThrow(()->new RuntimeException("[ Charmi ] Role is not found."));
+                roles.add(userRole);
+            } else {
+                strRoles.forEach(role->{
+                    enumRole tmpEnumRole=null;
+                    switch (role.getName()) {
+                        case ROLE_ADMIN:
+                            tmpEnumRole=enumRole.ROLE_ADMIN; break;
+                        default:
+                            tmpEnumRole=enumRole.ROLE_USER;
+                    }
+                    Role userRole=roleDao.findByName(tmpEnumRole)
+                            .orElseThrow(()->new RuntimeException("[ Charmi ] Role is not found."));
+                    roles.add(userRole);
+                });
+            }
+            user.setRoleList(roles);
+            userDao.insertUser(user);
+        }
+        map.put("message", Msg);
+        return ResponseEntity.status(httpStatus).body(map);
+    }
 }
