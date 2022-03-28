000144c3df11e22c4f0367f50f865c7bcea76a74
kent <s9n9201@yahoo.com.tw>
2022-03-28 14:04:37 +0800

    add @Service

--- src/main/java/web/charmi/security/service/UserDetailsServiceImp.java
+++ src/main/java/web/charmi/security/service/UserDetailsServiceImp.java
@@ -4,9 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import web.charmi.dao.UserDao;
 import web.charmi.entity.User;
-
+@Service
 public class UserDetailsServiceImp implements UserDetailsService {
     @Autowired
     private UserDao userDao;
