6a180c4a48351167580c2fe78bb8e803c7f5f121
kent <s9n9201@yahoo.com.tw>
2022-03-29 13:57:00 +0800

    add response role data

--- src/main/java/web/charmi/rowmapper/UserRowMapper.java
+++ src/main/java/web/charmi/rowmapper/UserRowMapper.java
@@ -5,17 +5,27 @@ import web.charmi.entity.User;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserRowMapper implements RowMapper<User> {
     @Override
     public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+        String RoleNameStr="", RoleNameArr[]=null;
+        List<String> roles=new ArrayList<>();
         User user=new User();
-
+        RoleNameStr=rs.getString("RoleName");
+        if (!RoleNameStr.equals("")) {
+            RoleNameArr=RoleNameStr.split(",");
+            for (int i=0; i<RoleNameArr.length; i++) {
+                roles.add(RoleNameArr[i]);
+            }
+        }
         user.setOrgId(rs.getInt("OrgId"));
         user.setOrgName(rs.getString("OrgName"));
         user.setEmail(rs.getString("Email"));
         user.setPassword(rs.getString("Password"));
-
+        user.setStrRoleList(roles);
         return user;
     }
 }
