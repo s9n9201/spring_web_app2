8d908c5ac9690ae21b41002b061d4750023c2469
kent <s9n9201@yahoo.com.tw>
2022-04-01 19:49:21 +0800

    response error message

--- src/main/java/web/charmi/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/web/charmi/security/jwt/AuthEntryPointJwt.java
@@ -1,7 +1,9 @@
 package web.charmi.security.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -10,12 +12,25 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
     private static final Logger log=LoggerFactory.getLogger(AuthEntryPointJwt.class);
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         log.error("[ Charmi ] Unauthorized: {}", authException.getMessage());
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: 帳號密碼錯誤!");
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        final Map<String, Object> body=new HashMap<>();
+        body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+        body.put("error","Unauthorized");
+        body.put("message","帳號密碼錯誤！");
+        body.put("path",request.getServletPath());
+        final ObjectMapper mapper=new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), body);
     }
 }
