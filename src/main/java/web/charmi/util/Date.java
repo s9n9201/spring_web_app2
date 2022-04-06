e15e38d830cc3b0c746b2e2339c14844be291bd6
kent <s9n9201@yahoo.com.tw>
2022-04-06 20:46:29 +0800

    add Instant to String method

--- src/main/java/web/charmi/util/Date.java
+++ src/main/java/web/charmi/util/Date.java
@@ -3,21 +3,25 @@ package web.charmi.util;
 import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 @Component
 public class Date {
 
     SimpleDateFormat dateFormat_1;
+    DateTimeFormatter DTF;
+
     public Date() {
         this.dateFormat_1=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        this.DTF=DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").withZone(ZoneId.of("Asia/Taipei"));
 
 
     }
     public String Now() {
-        DateTimeFormatter DTF=DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        return DTF.format(LocalDateTime.now());
+        return this.DTF.format(LocalDateTime.now());
     }
 
     public java.util.Date CDate(String str) throws Exception {
@@ -32,8 +36,11 @@ public class Date {
         } else {
             TimeStr="00:00:00";
         }
-
         java.util.Date date=dateFormat_1.parse(DateStr+" "+TimeStr);
         return date;
+    }
+
+    public String DateStr(Instant date) {
+        return this.DTF.format(date);
     }
 }
