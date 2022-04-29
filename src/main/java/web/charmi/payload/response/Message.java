f58c35ca1a75a3a064aa63c906038f3cc8a6b73a
kent <s9n9201@yahoo.com.tw>
2022-04-29 17:39:24 +0800

    增加recId欄位

--- src/main/java/web/charmi/payload/response/Message.java
+++ src/main/java/web/charmi/payload/response/Message.java
@@ -5,14 +5,16 @@ import org.springframework.http.HttpStatus;
 public class Message {
     private Integer status;
     private String message;
+    private Integer recId;
 
-    public Message(String message, Integer status) {
-        this.message=message;
-        this.status=status;
-    }
     public Message(String message, HttpStatus status) {
         this.message=message;
         this.status=status.value();
+    }
+    public Message(String message, HttpStatus status, Integer recId) {
+        this.message=message;
+        this.status=status.value();
+        this.recId=recId;
     }
 
     public Integer getStatus() {
@@ -29,5 +31,13 @@ public class Message {
 
     public void setMessage(String message) {
         this.message=message;
+    }
+
+    public Integer getRecId() {
+        return recId;
+    }
+
+    public void setRecId(Integer recId) {
+        this.recId=recId;
     }
 }
